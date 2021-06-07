53d19dd0623224d0f1d33f058159ba5e3a2cc0bf
ngatyah <davidngatyah@gmail.com>
2021-06-07 13:29:12 +0300

    adding the cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 const MealItem = props => {
 
     const price = `$${props.price.toFixed(2)}`
@@ -7,6 +8,9 @@ const MealItem = props => {
             <div className={classes['meal h3']}><h3>{props.name}</h3></div>
             <div className={classes.description}><p>{props.description}</p></div>
             <div className={classes.price}>{price}</div>
+        </div>
+        <div>
+            <MealItemForm id={props.id} />
         </div>
     </li>
 };
