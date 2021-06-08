9323d5b64ce4bb61bd3da5b325aef8432f7bb7c8
ngatyah <davidngatyah@gmail.com>
2021-06-08 17:54:43 +0300

    ad

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,12 +9,21 @@ const Cart = props => {
     const totalAmount = cartCtx.totalAmount.toFixed(2);
     const hasItems = cartCtx.items.length > 0;
 
+    const cartItemRemoveHandler = id => {
+
+    }
+    const cartItemAddHandler = item => {
+
+    }
+
     const cartItems = <ul>{
         cartCtx.items.map(item => <CartItem
             key={item.id}
             price={item.price}
             amount={item.amount}
             name={item.name}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
         />)}</ul>
     return <Modal onclose={props.onHideCart}>
         {cartItems}
